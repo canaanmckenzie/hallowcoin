8a3033b2eb1dc0015c576cf4611ef8a49c3dab3d
canaanmckenzie <cjmckenz@uvm.edu>
2018-11-01 14:08:44 -0400

    add websocket error handler

--- p2p.ts
+++ p2p.ts
@@ -1,5 +1,4 @@
 //peer to peer server
-//
 import * as Websocket from 'ws';
 import {Server} from 'ws';
 
@@ -7,7 +6,7 @@ import {add_block_to_chain, Block, get_blockchain, get_latest_block,is_valid_blo
 
 const sockets: Websocket[] = [];
 
-//don't really need to the numbering
+//don't really need to do the numbering
 enum message_type {
 	QUERY_LATEST = 0,
 	QUERY_ALL = 1,
@@ -36,5 +35,70 @@ const init_connection = (ws: Websocket) => {
 	write(ws, query_chain_length_message());
 };
 
+//decode message from websocket connection
 const write = (ws: Websocket, message: Message): void => ws.send(JSON.stringify(message));
 
+//handle response from websocket
+const JSON_to_Object = <T>(data: string): T => {
+	try {
+		return JSON.parse(data);
+	} catch (e) {
+		console.log(e);
+		return null;
+	}
+};
+
+const init_message_handler = (ws: WebSocket) => {
+	ws.on('message',(data:string) => {
+		const message: Message = JSON_to_Object<Message>(data);
+		if(message === null){
+			console.log('could not parse received JSON message ' + data);
+			return;
+		}
+		console.log('Received Message ' + JSON.stringify(message));
+		switch(message.type) {
+			case message_type.QUERY_LATEST:
+				write(ws,response_latest_msg());
+				break;
+			case message_type.QUERY_ALL:
+				write(ws,response_latest_msg());
+				break;
+			case message_type.RESPONSE_BLOCKCHAIN:
+				const received_blocks: Block[] = JSON_to_Object<Block[]>(message.data);
+				if (received_blocks === null){
+					console.log('invalid blocks received: ');
+					console.log(message.data);
+					break;
+				}
+				handle_blockchain_response(received_blocks);
+				break;
+		}
+		});
+};
+
+
+//broadcast to peers
+const broadcast = (message: Message): void => sockets.forEach((socket) => write(socket,message));
+
+
+//messages
+const query_chain_length_message = (): Message => ({
+	'type':message_type.QUERY_LATEST,
+	'data':null});
+const query_all_message = (): Message => ({
+	'type':message_type.QUERY_ALL,
+	'data':null});
+const response_latest_msg = (): Message => ({
+	'type':message_type.RESPONSE_BLOCKCHAIN,
+	'data':JSON.stringify([get_latest_block])
+});
+
+
+const init_error_handler = (ws: WebSocket) => {
+	const close_connection = (ws: Websocket) => {
+		console.log('connection failed to peer: ' +ws.url);
+		sockets.splice(sockets.indexOf(myWs), 1);
+	};
+
+	
+}
